eb66158a21d36194ef68c4927b3b74a24982ff9a
MiniBill <minibill@sina.com>
2019-11-28 17:47:57 +0800

    2019-11-28 更新

--- 细节优化-190520不再更新/工序产能/style/ProcessCapacity.js
+++ 细节优化-190520不再更新/工序产能/style/ProcessCapacity.js
@@ -27,17 +27,21 @@
         },
         //总表条件查询
         onQuerySubmit() {
+            this.loading = true;
             axios.post('/Process_Capacity/TotalProcess_Capacity', {
                 protype: this.queryTable.protype,
                 proplatform: this.queryTable.proplatform
             }).then(res => {
-                console.log(res.data);
+                console.log(JSON.parse(JSON.stringify(res.data)));
+                this.tableList = res.data;
+                this.loading = false;
                 this.$message({
                     message: '查询成功！',
                     type: 'success'
                 });
             }).catch(err => {
                 console.warn("查询数据失败");
+                this.loading = false;
             });
         }
     }
