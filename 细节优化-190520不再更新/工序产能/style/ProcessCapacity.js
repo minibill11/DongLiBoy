a9da3bacc0fe7d3f1ac40c2a5cc2ca06f07d3fa1
MiniBill <minibill@sina.com>
2019-11-29 17:30:59 +0800

    2019-11-29 更新

--- 细节优化-190520不再更新/工序产能/style/ProcessCapacity.js
+++ 细节优化-190520不再更新/工序产能/style/ProcessCapacity.js
@@ -3,13 +3,20 @@
         return {
             loading: false,
             tableList: [],
+            queryTable: {
+                protype: "",
+                proplatform: ""
+            },
+            options: {
+                protype: [],
+                proplatform: []
+            }
         }
     },
     created: function () {
-        this.getTableData();
-    },
-    mounted: function () {
-
+        //this.getTableData();
+        this.getProtype();
+        this.getPlatfrom();
     },
     methods: {
         //总表数据显示
@@ -43,6 +50,29 @@
                 console.warn("查询数据失败");
                 this.loading = false;
             });
-        }
-    }
+        },
+        //型号下拉列表
+        getProtype() {
+            axios.post('/Process_Capacity/TypeList').then(res => {
+                //console.log(res.data);
+                this.options.protype = res.data;
+            }).catch(err => {
+                console.warn("型号列表获取失败");
+            });
+        },
+        //平台下拉列表
+        getPlatfrom() {
+            axios.post('/Process_Capacity/PlatfromList').then(res => {
+                //console.log(res.data);
+                this.options.proplatform = res.data;
+            }).catch(err => {
+                console.warn("型号列表获取失败");
+            });
+        },
+    },
+    //watch: {
+    //    "queryTable.proplatform"(v) {
+    //        this.onQuerySubmit(v);
+    //    }
+    //}
 }
